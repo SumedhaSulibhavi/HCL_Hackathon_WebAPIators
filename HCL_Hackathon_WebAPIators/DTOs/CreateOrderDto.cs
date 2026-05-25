63979ef41bdb7fe0ebce6b3c16d2908316cfb3fd
YashaswiniKurabet <kurabetjayashree@gmail.com>
2026-05-25 13:55:37 +0530

    [P3] feat: CreateOrderDto - order creation validation dto

---
+++ HCL_Hackathon_WebAPIators/DTOs/CreateOrderDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HCL_Hackathon_WebAPIators.DTOs
+{
+    public class CreateOrderDto
+    {
+        [Required]
+        [Range(1, 100000)]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string ItemName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        public string Category { get; set; } = string.Empty;
+    }
+}
