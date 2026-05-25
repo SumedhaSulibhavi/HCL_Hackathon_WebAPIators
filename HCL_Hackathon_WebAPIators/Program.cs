2082daea0a5904f84352b517b84ade54e1d66e38
Sanjana Patil <patilsanjana197@gmail.com>
2026-05-25 14:49:56 +0530

    Sanjana: clear local compilation errors and finalize web infrastructure configuration

--- HCL_Hackathon_WebAPIators/Program.cs
+++ HCL_Hackathon_WebAPIators/Program.cs
@@ -1,12 +1,18 @@
 using HCL_Hackathon_WebAPIators.Helpers;
+using HCL_Hackathon_WebAPlators.Data;
+using HCL_Hackathon_WebAPlators.Helpers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi;
 using Microsoft.OpenApi.Models;
 using System.Text;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// 0. Register Database Engine with Connection Properties
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // 1. Configure JSON serialization settings to ignore circular references globally
 builder.Services.AddControllers().AddJsonOptions(options => {
